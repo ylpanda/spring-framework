d9ee9a89722e7a5ff63bb45f100712ab8f9bf380
ylpanda <aukovyps@163.com>
2020-09-15 23:45:34 +0800

    add spring-ylpanda module

---
+++ spring-ylpanda/src/main/java/com/ylpanda/spring/config/AnnotationConfig.java
@@ -0,0 +1,11 @@
+package com.ylpanda.spring.config;
+
+/**
+ * @author ylpanda
+ * @create 2020/9/15 23:44
+ * @package com.ylpanda.spring.config
+ * @since 1.0.0
+ */
+public class AnnotationConfig {
+
+}
